147e1a7f3194495e2109780a7fc2e7af4b9f064a
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-04-17 23:08:03 +0100

    intial commit

---
+++ lib/pages/fisrt_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class fisrt_page extends StatelessWidget {
+  const fisrt_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/img/pexels-myfoodie-2664443.jpg'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text.rich(
+                  TextSpan(
+                    style: TextStyle(fontSize: 30),
+                    children: [
+                      TextSpan(
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        text: 'No Worry,',
+                      ),
+                      TextSpan(text: 'Handel your '),
+                    ],
+                  ),
+                ),
+                Text.rich(
+                  style: TextStyle(fontSize: 30),
+                  TextSpan(
+                    children: [
+                      TextSpan(text: 'watsed food with '),
+                      TextSpan(
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        text: 'MobiltyX.',
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "One App to Save Them All – Fight Food Waste!",
+                  style: TextStyle(fontSize: 15),
+                ),
+                SizedBox(height: 60),
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.amberAccent,
+                    ),
+                    height: 70,
+                    child: Center(
+                      child: Text(
+                        'Get Started  >',
+                        style: TextStyle(
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
